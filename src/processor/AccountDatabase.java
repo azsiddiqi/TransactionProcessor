0cf01145fb49d5141f13eb7c16a1170722269641
patel <karan.patel4@rutgers.edu>
2022-02-18 22:09:50 -0500

    filled methods for accountdatabase

--- src/processor/AccountDatabase.java
+++ src/processor/AccountDatabase.java
@@ -26,10 +26,41 @@ public class AccountDatabase {
         }
         accounts = increasedSize;
     }
-    public boolean open(Account account) { }
-    public boolean close(Account account) { }
-    public void deposit(Account account) { }
+
+    public boolean open(Account account) {
+        for (int i = 0; i < accounts.length; i++) {
+            if (accounts[i].equals(account)) {
+                return false;
+            }
+        }
+        if (numAcct == accounts.length) {
+            grow();
+        }
+        accounts[numAcct] = account;
+        numAcct++;
+        return true;
+    }
+
+    public boolean close(Account account) {
+        int removedAcctIndex = find(account);
+        if (removedAcctIndex == NOT_FOUND) {
+            return false;
+        }
+        for (int j = removedAcctIndex + 1; j < numAcct; j++) {
+                accounts[j - 1] = accounts[j];
+        }
+        accounts[numAcct - 1] = null;
+        return true;
+    }
+
+    public void deposit(Account account) {
+    Account one = find(account);
+    if (one != -1){
+        one.deposit(account.balance);
+    }
+    }
     public boolean withdraw(Account account) { } //return false if insufficient fund
+
     public void print() {
         for (int i = 0; i < numAcct; i++){
             System.out.println(accounts[i].toString());
